f71219b30c491be888210cfc481d0bbc48066b44
samckp <sam.mca.liet@gmail.com>
2019-08-11 22:32:24 +0530

    Code update, Camel Unzip logic added.

--- src/main/java/com/sam/camel/poc/route/UnzipRoute.java
+++ src/main/java/com/sam/camel/poc/route/UnzipRoute.java
@@ -2,15 +2,21 @@ package com.sam.camel.poc.route;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.zipfile.ZipSplitter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UnzipRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
-        from("")
+        from("{{fromZipRoute}}")
                 .log(LoggingLevel.INFO, "${body}")
-                .to("")
+                .split(new ZipSplitter())
+                .streaming()
+                .convertBodyTo(String.class)
+                .to("{{toZipRoute}}")
                 ;
     }
 }
